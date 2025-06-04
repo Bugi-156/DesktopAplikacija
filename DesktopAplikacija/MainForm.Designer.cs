6c0432d8cdc46e36c0a5202f55030b911ed35816
38597 <38597@ASPIRE7>
2025-06-04 22:25:40 +0200

    Add project files.

---
+++ DesktopAplikacija/MainForm.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace DesktopAplikacija
+{
+    partial class MainForm
+    {
+        
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtKontakt = new System.Windows.Forms.TextBox();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnUcitaj = new System.Windows.Forms.Button();
+            this.btnSpremi = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Prezime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Kontakt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(56, 45);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(100, 20);
+            this.txtIme.TabIndex = 0;
+            // 
+            // txtKontakt
+            // 
+            this.txtKontakt.Location = new System.Drawing.Point(56, 97);
+            this.txtKontakt.Name = "txtKontakt";
+            this.txtKontakt.Size = new System.Drawing.Size(100, 20);
+            this.txtKontakt.TabIndex = 1;
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(56, 71);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(100, 20);
+            this.txtPrezime.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Ime,
+            this.Prezime,
+            this.Kontakt});
+            this.dataGridView1.Location = new System.Drawing.Point(330, 83);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(442, 202);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // btnUcitaj
+            // 
+            this.btnUcitaj.Location = new System.Drawing.Point(152, 209);
+            this.btnUcitaj.Name = "btnUcitaj";
+            this.btnUcitaj.Size = new System.Drawing.Size(75, 23);
+            this.btnUcitaj.TabIndex = 4;
+            this.btnUcitaj.Text = "Učitaj";
+            this.btnUcitaj.UseVisualStyleBackColor = true;
+            this.btnUcitaj.Click += new System.EventHandler(this.btnUcitaj_Click);
+            // 
+            // btnSpremi
+            // 
+            this.btnSpremi.Location = new System.Drawing.Point(56, 209);
+            this.btnSpremi.Name = "btnSpremi";
+            this.btnSpremi.Size = new System.Drawing.Size(75, 23);
+            this.btnSpremi.TabIndex = 5;
+            this.btnSpremi.Text = "Spremi";
+            this.btnSpremi.UseVisualStyleBackColor = true;
+            this.btnSpremi.Click += new System.EventHandler(this.btnSpremi_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ime";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Prezime";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Kontakt";
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // Ime
+            // 
+            this.Ime.HeaderText = "Ime";
+            this.Ime.Name = "Ime";
+            // 
+            // Prezime
+            // 
+            this.Prezime.HeaderText = "Prezime";
+            this.Prezime.Name = "Prezime";
+            // 
+            // Kontakt
+            // 
+            this.Kontakt.HeaderText = "Kontakt";
+            this.Kontakt.Name = "Kontakt";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSpremi);
+            this.Controls.Add(this.btnUcitaj);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.txtKontakt);
+            this.Controls.Add(this.txtIme);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.TextBox txtKontakt;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnUcitaj;
+        private System.Windows.Forms.Button btnSpremi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Prezime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Kontakt;
+    }
+}
+
---
+++ DesktopAplikacija/MainForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace DesktopAplikacija
+{
+    public partial class MainForm : Form
+    {
+        private List<Korisnik> lista = new List<Korisnik>();
+        private readonly string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;
+                                      AttachDbFilename=C:\Users\38597\source\repos\DesktopAplikacija\DesktopAplikacija\KorisniciBaza.mdf;
+                                      Integrated Security=True;Connect Timeout=30";
+
+        public MainForm()
+        {
+            InitializeComponent();
+            dataGridView1.CellDoubleClick += DataGridView1_CellDoubleClick;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnSpremi_Click(object sender, EventArgs e)
+        {
+            string ime = txtIme.Text.Trim();
+            string prezime = txtPrezime.Text.Trim();
+            string kontakt = txtKontakt.Text.Trim();
+            if (string.IsNullOrEmpty(ime) || string.IsNullOrEmpty(prezime) || string.IsNullOrEmpty(kontakt))
+            {
+                MessageBox.Show("Sva polja moraju biti ispunjena!", "Upozorenje", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            using (SqlConnection conn = new SqlConnection(cs))
+            {
+                string query = "INSERT INTO Korisnici (Ime, Prezime, Kontakt) VALUES (@Ime, @Prezime, @Kontakt)";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Ime", ime);
+                    cmd.Parameters.AddWithValue("@Prezime", prezime);
+                    cmd.Parameters.AddWithValue("@Kontakt", kontakt);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            MessageBox.Show("Korisnik je spremljen u bazu.", "Uspjeh", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            txtIme.Text = "";
+            txtPrezime.Text = "";
+            txtKontakt.Text = "";
+        }
+
+        private void btnUcitaj_Click(object sender, EventArgs e)
+        {
+            lista.Clear();
+            using (SqlConnection conn = new SqlConnection(cs))
+            {
+                conn.Open();
+                string sql = "SELECT ID, Ime, Prezime, Kontakt FROM Korisnici";
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int id = reader.GetInt32(0);
+                        string ime = reader.GetString(1);
+                        string prezime = reader.GetString(2);
+                        string kontakt = reader.GetString(3);
+                        Korisnik k = new Korisnik(id, ime, prezime, kontakt);
+                        lista.Add(k);
+                    }
+                }
+            }
+            dataGridView1.Columns.Clear();
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = lista;
+            if (dataGridView1.Columns["Kontakt"] != null)
+                dataGridView1.Columns["Kontakt"].Visible = false;
+            if (dataGridView1.Columns["Prezime"] != null)
+                dataGridView1.Columns["Prezime"].Visible = false;
+        }
+
+
+        private void DataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+            Korisnik odabrani = dataGridView1.Rows[e.RowIndex].DataBoundItem as Korisnik;
+            if (odabrani != null)
+            {
+                string naslov = $"Podatci za {odabrani.Ime}";
+                string poruka = $"ID: {odabrani.ID}\r\nIme: {odabrani.Ime}\r\nPrezime: {odabrani.Prezime}\r\nKontakt: {odabrani.Kontakt}";
+                MessageBox.Show(poruka, naslov, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
